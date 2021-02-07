a9f5c9f1313bab5c2012f4648b328e600b48d4cc
Cody Swendrowski <cody@ironmoosedevelopment.com>
2021-02-06 18:42:07 -0600

    Add way to get Profile ID from Name

---
+++ SteamShip/Server/Controllers/SteamController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SteamShip.Shared;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace SteamShip.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SteamController : ControllerBase
+    {
+
+        private const string ApiKey = "B809FE9D19152246D16A66E7ECE22ADF";
+        private readonly ILogger<SteamController> _logger;
+
+        public SteamController(ILogger<SteamController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public SteamProfile GetSteamProfile(string id)
+        {
+            var steamIdRegex = new Regex(@"\d{17}");
+
+            if (!steamIdRegex.IsMatch(id))
+            {
+                id = GetSteamIdFromName(id);
+            }
+
+            return new SteamProfile()
+            {
+                Id = id
+            };
+        }
+
+        private string GetSteamIdFromName(string name)
+        {
+            var steamCommunityProfileUrl = $"https://steamcommunity.com/id/{name}/?xml=1";
+
+            try
+            {
+                var xml = XDocument.Load(steamCommunityProfileUrl);
+                return xml.Element("profile").Element("steamID64").Value;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SteamShip/Shared/SteamProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamShip.Shared
+{
+    public class SteamProfile
+    {
+        public string Id { get; set; }
+    }
+}
